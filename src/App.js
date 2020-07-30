937be7c185c6e80e731c76cd059dbf63fada3d40
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-07-30 10:39:34 -0400

    creating home and not found pages to clean up app.js and make sure all endpoints are accounted for

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
+import Home from './components/pages/Home';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import User from './components/users/User';
+import NotFound from './components/pages/NotFound';
+
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+
 import './App.css';
 
 const App = () => {
@@ -18,20 +20,12 @@ const App = () => {
           <div className='App'>
             <Navbar />
             <div className='container'>
+              <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => (
-                    <Fragment>
-                      <Alert />
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                ></Route>
+                <Route exact path='/' component={Home}></Route>
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
